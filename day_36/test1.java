e93802f453f0ca9045bf10fb0d97d01a0cba7374
dadayangyu <2498090141@qq.com>
2021-06-07 22:23:37 +0800

    day_36

---
+++ day_36/test1.java
@@ -0,0 +1,19 @@
+package day_36;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test1 {
+    /**
+     * 题目:Rational Arithmetic
+     * 题目描述:编写一个简单的两个数的计算器,进行简单的加减乘除计算
+     * 难   点:要保证对无理数的处理
+     * 输入:5/3 0/6
+     * 输出:1 2/3 + 0 =1 2/3
+     *     1 2/3 - 0 =1 2/3
+     *     1 2/3 * 0 =1 2/3
+     *     1 2/3 / 0 =Inf
+    * */
+   
+}
